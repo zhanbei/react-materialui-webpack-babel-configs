66f2e754234dffe43f56f472223f2f2da01e9677
Fisher <fisher.subs@gmail.com>
2018-05-05 19:47:23 +0800

    [diff-project.sh] Added script to diff ./template and the project(parent folder).
    - One-way diff files from the `template` folder to those in the parent folder.

---
+++ diff-project.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+#
+# Copyright 2018. All rights reserved.
+#
+# One-way diff files from the `template` folder to those in the parent folder.
+#
+# - Output the names of files in the `template` folder that are different from those in the parent folder.
+# - Output the names of new files in the `template` folder comparing to the parent folder.
+# - New files in the parent folder are ignored.
+#
+
+cd "${BASH_SOURCE%/*}";
+
+# @see https://stackoverflow.com/questions/16787916/difference-between-two-directories-in-linux
+diff -qr ./template .. | grep './template'
