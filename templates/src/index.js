4008d366937979246d7ffab698b77e449fe28a47
Fisher <fisher.subs@gmail.com>
2018-05-05 22:03:01 +0800

    [template/src/index] Added the project entry script.

---
+++ templates/src/index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {MuiThemeProvider, createMuiTheme} from 'material-ui/styles';
+import App from './App';
+
+const AppTheme = require('./resources/AppTheme');
+const muiTheme = createMuiTheme(AppTheme);
+
+// The Webpack entrance.
+ReactDOM.render((
+		<MuiThemeProvider theme={muiTheme}>
+			<App/>
+		</MuiThemeProvider>
+	), document.getElementById('root')
+);
